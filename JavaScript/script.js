42e51c48694f8b957955456b7fc122c950e6e0aa
Neelabh-sharma <neelabhsharma2002@gmail.com>
2023-05-21 00:23:42 +0530

    commit 21/may/Aos

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+
+//Animation on scrolling 
+// setting time , offset and Delays 
+AOS.init({
+    offset: 200, // offset (in px) from the original trigger point
+    delay: 500, // values from 0 to 3000, with step 50ms
+    duration: 2000, // values from 0 to 3000, with step 50ms
+});
+ //
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const img = document.querySelectorAll("img");
+prev.addEventListener('click',() =>{
+    
+});
